0e65bc3991ec53b4cf2c50d783628dadad194fa5
Jerry Gerou <jerrysgerou@gmail.com>
2018-10-08 18:47:13 -0500

    Add mission list view and template

--- missions_possible/missions/views.py
+++ missions_possible/missions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class MissionIndexView(generic.ListView):
+    """
+    A view to display all existing missions.
+    """
+    template_name = 'missions/index.html'
+    context_object_name = 'mission_list'
+
+    def get_queryset(self):
+        return Mission.objects.all()
